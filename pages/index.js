ca43eead93b2712173bfe70cc3c52e128dd66ed6
José Carlos Haza <acremur@gmail.com>
2021-12-19 21:04:55 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
-import SmallCard from '../components/smallCard'
+// import SmallCard from '../components/smallCard'
 
 export default function Home({ exploreData, cardsData }) {
   return (
